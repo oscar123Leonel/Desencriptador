865a4a65e7f2b9e6568d35f2f018a117caa223cf
oscar123Leonel <escobarbaides1995@gmail.com>
2017-04-05 18:49:31 -0300

    Add files via upload
    Generacion de String para Biblioteca de datos

---
+++ palabras/palabras/palabras/Conexion.cs
@@ -0,0 +1,109 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SportSystem.Controlador
+{
+    class Conexion
+    {
+        private MySqlCommandBuilder cmb;
+        public DataSet ds = new DataSet();
+        public MySqlDataAdapter da;
+        public MySqlCommand comando;
+        public String CadenaConexion = "Server=localhost;Port=3306;Database=auditoria;Uid=root;Password=admin;";
+        protected MySqlConnection con = new MySqlConnection();
+
+        public void conectar1()
+        {
+
+            con.ConnectionString = CadenaConexion;
+            try
+            {
+                con.Open();
+            }
+            catch
+            {
+                MessageBox.Show("error de conexion");
+
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+        public void consulta(string sql, string tabla)
+        {
+
+            try
+            {
+                ds.Tables.Clear();
+                da = new MySqlDataAdapter(sql, con);
+                cmb = new MySqlCommandBuilder(da);
+                da.Fill(ds, tabla);
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+
+        public Boolean insertar(String sql)
+        {
+            con.Open();
+            comando = new MySqlCommand(sql, con);
+            int i = comando.ExecuteNonQuery();
+            con.Close();
+            if (i > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public bool eliminar(string tabla, string condicion)
+        {
+            con.Open();
+            string elimina = "delete from " + tabla + " where " + condicion;
+            comando = new MySqlCommand(elimina, con);
+            int i = comando.ExecuteNonQuery();
+            con.Close();
+            if (i > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public bool actualizar(string tabla, string campos, string condicion)
+        {
+            con.Open();
+            string actualizar = "update " + tabla + " set " + campos + " where " + condicion;
+            comando = new MySqlCommand(actualizar, con);
+            int i = comando.ExecuteNonQuery();
+            con.Close();
+            if (i > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ palabras/palabras/palabras/Form1.cs
@@ -0,0 +1,52 @@
+﻿using SportSystem.Controlador;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace palabras
+{
+    
+    public partial class Form1 : Form
+    {
+     
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random nuevaLetra = new Random();
+            string comillas = "\"";
+            string diagonal = "\\";
+            string posiblesletras = "!#$%&'()*+,-./:;<=>?_^][@¿¡|°abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXWZ0123456789"+comillas+diagonal;
+            int longitud = posiblesletras.Length;
+            char letra;
+            int longitudnuevacadena = 15; // aqui se define la longitud de la nueva cadena ha crear 
+            string nuevacadena = "";
+            txtpalabra.Text = "";
+            for (int i = 0; i < longitudnuevacadena; i++)
+                {
+                   letra = posiblesletras[nuevaLetra.Next(longitud)];
+                   nuevacadena += letra.ToString();
+                }
+            txtpalabra.Text = nuevacadena;         
+            
+        }
+
+        private void insert()
+        { 
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
